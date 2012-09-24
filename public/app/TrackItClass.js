eb26790475521d6f3c6e5d8b4129f3eb2464f920
Marcus Krejpowicz <krebbl@gmail.com>
2012-09-24 19:06:18 +0200

    added interface for creating and listing projects ...

--- public/app/TrackItClass.js
+++ public/app/TrackItClass.js
@@ -1,24 +1,52 @@
 define(
-    ["js/core/Application"],
-    function (Application) {
+    ["js/core/Application","app/model/Project","js/data/DataSource", "flow", "js/data/Collection"],
+    function (Application, Project, DataSource, flow, Collection) {
 
         return Application.inherit({
-            /**
-             *  initializes the application variables
-             */
-            initialize:function () {
-                this.set('appName','TrackIt');
+
+            defaults: {
+                project: null,
+                projects: Array
             },
+            inject: {
+                api: DataSource
+            },
+
             /***
              * Starts the application
              * @param parameter
              * @param callback
              */
             start:function (parameter, callback) {
-                // false - disables autostart
-                this.callBase(parameter, false);
 
-                callback();
+                var self = this;
+                var projects = this.$.api.createCollection(Collection.of(Project), {});
+                this.set('projects', projects);
+                this.set('project', projects.createItem());
+                flow()
+                    .seq(function(cb){
+                        projects.fetch(null, cb);
+                    })
+                    .exec(function(err){
+                        if (err) {
+                            callback(err);
+                        } else {
+                            // call start from super
+                            self.start.baseImplementation.call(self, parameter, callback);
+                        }
+                    });
+            },
+            _handleProjectSubmit: function(e){
+                var self = this;
+                this.$.project.save(null, function(err, project){
+                    if(!err){
+                        self.$.projects.add(project);
+                        self.set('project', self.$.projects.createItem());
+                    }else{
+                        self.log(err);
+                    }
+                });
+                e.preventDefault();
             }
         });
     }
